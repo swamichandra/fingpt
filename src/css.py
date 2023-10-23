f17f810ff1c584581c21ba5e539f08eac380bfbf
Swami Chandrasekaran <swamichandra@gmail.com>
2023-10-23 14:42:24 -0500

    Update and rename src/css.py to src/pages/css.py

--- src/css.py
+++
@@ -1,7 +0,0 @@
-v1 = """
-@import url('https://fonts.googleapis.com/css2?family=Schibsted+Grotesk&display=swap');
-
-html, body, [class*="css"] {
-    font-family: 'Schibsted Grotesk', sans-serif;
-}
-"""
---
+++ src/pages/css.py
@@ -0,0 +1,7 @@
+v1 = """
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed&display=swap');
+
+html, body, [class*="css"] {
+    font-family: 'IBM Plex Sans Condensed', sans-serif;
+}
+"""
