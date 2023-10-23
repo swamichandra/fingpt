e58c1c1e9ef732c13081e81968a3ddd031ec80ea
Swami Chandrasekaran <swamichandra@gmail.com>
2023-10-23 17:21:28 -0500

    Update finmetrics.py

--- src/pages/finmetrics.py
+++ src/pages/finmetrics.py
@@ -9,7 +9,7 @@ import css
 
 st.set_page_config(page_title="Financial Insights Companion", page_icon="📊", layout="wide", initial_sidebar_state="collapsed")
 st.write(f'<style>{css.v1}</style>', unsafe_allow_html=True)
-st.title("📊 Finance Metrics Review")
+st.title("📊 Finance Insights Companion")
 st.info("""
 Pick your desired company's ticker symbol, toggle the required financial insights, and hit Generate Insights. Wait a few moments for the system to compile the data and insights tailored to the selected company. Download a comprehensive PDF report.
 """)
