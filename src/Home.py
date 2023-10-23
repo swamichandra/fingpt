548b8f5ab8b0b90c825077e274137c2df032e769
Swami Chandrasekaran <swamichandra@gmail.com>
2023-10-23 15:57:55 -0500

    Update Home.py

--- src/Home.py
+++ src/Home.py
@@ -5,8 +5,8 @@ script_dir = Path(__file__).resolve().parent
 project_root = script_dir.parent
 sys.path.append(str(project_root))
 
-st.info(str(script_dir))
-st.info(str(project_root))
+print(str(script_dir))
+print(str(project_root))
 
 import streamlit as st
 
