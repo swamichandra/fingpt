a157c4668ccd1e80e5b4066abe64bd5de74bfbb7
Swami Chandrasekaran <swamichandra@gmail.com>
2023-10-23 14:38:47 -0500

    Update Home.py

--- src/Home.py
+++ src/Home.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import css
 script_dir = Path(__file__).resolve().parent
 project_root = script_dir.parent
 sys.path.append(str(project_root))
@@ -8,22 +9,7 @@ import streamlit as st
 
 st.set_page_config(page_title="FinSight", page_icon=":money_with_wings:", layout="wide")
 
-st.markdown("""
-        <style>
-        @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed&display=swap');
-
-        html, body, [class*="css"] {
-            font-family: 'IBM Plex Sans Condensed', sans-serif;
-        }
-
-               .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 0rem;
-                    padding-left: 1rem;
-                    padding-right: 1rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
+st.write(f'<style>{css.v1}</style>', unsafe_allow_html=True)
 
 st.title(":money_with_wings: FinSight \n\n **Financial Insights at Your Fingertip**")
 
