0470d0ff2d873065f8b31a014d030e16511b1e2d
Swami Chandrasekaran <swamichandra@gmail.com>
2023-10-23 14:28:36 -0500

    Update Home.py

--- src/Home.py
+++ src/Home.py
@@ -8,6 +8,23 @@ import streamlit as st
 
 st.set_page_config(page_title="FinSight", page_icon=":money_with_wings:", layout="wide")
 
+st.markdown("""
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed&display=swap');
+
+        html, body, [class*="css"] {
+            font-family: 'IBM Plex Sans Condensed', sans-serif;
+        }
+
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 1rem;
+                    padding-right: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
 st.title(":money_with_wings: FinSight \n\n **Financial Insights at Your Fingertip**")
 
 st.balloons()
