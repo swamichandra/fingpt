a2af6b87829294cc0866a1cd57268b8910ce9ac6
Swami Chandrasekaran <swamichandra@gmail.com>
2023-10-23 14:31:50 -0500

    Create css.py

---
+++ src/css.py
@@ -0,0 +1,7 @@
+v1 = """
+@import url('https://fonts.googleapis.com/css2?family=Schibsted+Grotesk&display=swap');
+
+html, body, [class*="css"] {
+    font-family: 'Schibsted Grotesk', sans-serif;
+}
+"""
